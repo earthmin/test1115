bd4fb4734ad0bc58331aebdec3ef9127175dd5b3
earthmin <fltk4286@naver.com>
2024-11-15 11:24:39 +0900

    20241115

---
+++ springboot-test1115/test1115/src/main/java/com/korit/test1115/Test1115Application.java
@@ -0,0 +1,13 @@
+package com.korit.test1115;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test1115Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test1115Application.class, args);
+	}
+
+}
---
+++ springboot-test1115/test1115/src/test/java/com/korit/test1115/Test1115ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.korit.test1115;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test1115ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
